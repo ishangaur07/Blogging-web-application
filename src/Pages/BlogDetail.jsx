758c9da638a424deac2e53c4baa0be62aa213348
Ishan Gaur <ishangaur107@gmail.com>
2023-08-02 08:57:35 +0530

    Fetching blogs

--- src/Pages/BlogDetail.jsx
+++ src/Pages/BlogDetail.jsx
@@ -1,12 +1,45 @@
 import { useParams } from "react-router-dom";
-
+import React, {useState,useEffect} from "react";
+import firebase from '../firebase/FirebaseConfig'; 
+import { getDatabase, ref, onValue, off } from 'firebase/database';
 const BlogDetail = () => {
-    
     const params = useParams();
+    const [blog, setBlogs] = useState(null);
+  
+    useEffect(() => {
+      // Fetch data from Firebase when the component mounts
+      const db = getDatabase();
+      const dbRef = ref(db, 'blogs');
+  
+      const unsubscribe = onValue(dbRef, (snapshot) => {
+        // Convert the snapshot to an array of blogs
+        const blogsData = snapshot.val();
+        if (blogsData) {
+          const blog = blogsData[params.blogId];
+        setBlogs(blog);
+        }else{
+          setBlogs(null);
+        }
+      });
+  
+      // Unsubscribe from the Firebase database when the component unmounts
+      return () => {
+        // Clean up the database reference
+        off(dbRef, 'value', unsubscribe);
+      };
+    }, [params.blogId]);
+  
     return (
       <>
-      <h1>Product Details!</h1>
-      <p>{params.blogId}</p>
+      {blog ? (
+        <>
+        <h1>{blog.author}</h1>
+        <p>{blog.content}</p>
+  
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
       </>
     )
 
