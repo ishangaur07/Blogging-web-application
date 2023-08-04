8d777892d70170d9c1e396d366c48c3a3986e1a3
Ishan Gaur <ishangaur107@gmail.com>
2023-08-04 08:40:24 +0530

    working button

--- src/Components/BannerHome.jsx
+++ src/Components/BannerHome.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classes from './BannerHome.module.css';
 const BannerHome = ()=>{
     return <div className={classes.banner}>
         <div className={classes.leftContent}>
         <h1>Stay curious.</h1>
         <p>Discover stories, thinking, and expertise from writers on any topic.</p>
-        <button className={classes.btn}>Start Reading</button>
+       <Link to='/about-us'><button className={classes.btn}>Start Reading</button></Link>
         </div>
         <div className={classes.rightContent}>
         </div>
